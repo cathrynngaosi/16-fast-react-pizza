bfc2e97bb222d347f5d26648bec10bb10975896c
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-07-20 18:50:49 +0800

    Implement delete items from cart logic

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -47,3 +47,6 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityByID = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaID === id)?.quantity ?? 0;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
 import { formatCurrency } from "../../utilities/helpers";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityByID } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityByID(id));
+  const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
     const newItem = {
@@ -40,7 +43,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
+          {isInCart && <DeleteItem pizzaID={id} />}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
